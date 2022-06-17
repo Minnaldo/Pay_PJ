361a174c9116735ab25adf48894553667eddad2d
minnaldo <minnaldo6602@gmail.com>
2022-06-17 22:17:07 +0900

    init

---
+++ src/main/java/com/example/Pay_PJ/PayPjApplication.java
@@ -0,0 +1,14 @@
+package com.example.Pay_PJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayPjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayPjApplication.class, args);
+
+	}
+
+}
